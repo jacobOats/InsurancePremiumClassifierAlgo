0eda5ab99fa1be414b23f7bd9f5541d5acad7bb3
jacobOats <97692358+jacobOats@users.noreply.github.com>
2024-04-26 17:13:23 -0400

    Delete app directory

--- app/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('ml', views.getData),
-    path('vType', views.getVType),
-    path('vMake', views.getVMake),
-    path('vUsage', views.getVUsage)
-]
